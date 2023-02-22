af6755bdf838cdc463403f8700d0845476a37910
Gabries <105074547+gabrieszin@users.noreply.github.com>
2023-02-22 16:19:28 -0300

    Corrigindo bugs de troca de classes

--- assets/js/modulos/navegacao.js
+++ assets/js/modulos/navegacao.js
@@ -13,28 +13,53 @@
   })
   
   const verificarMudancaSecao = () => {
-    const secaoNavegadaURL = ((window.location.hash).toLocaleLowerCase()).replaceAll('#', '');
+    const secaoNavegadaURL = ((window.location.hash).toLocaleLowerCase()).replaceAll('#', '');    
+    const interacaoValida = secaoNavegadaURL !== '' && secoesRolagem.includes(secaoNavegadaURL);
+    
+    limparClassesItensSumario();
+
+    if(interacaoValida){
+      marcarItemAtivo(secaoNavegadaURL);
+    }
+  }
+  
+  window.addEventListener('scroll', () => {
+    const array = [
+      {secao: 'apresentacao', valor: document.querySelector('header.cabecalho').getBoundingClientRect().top},
+      {secao: 'funcoes', valor: document.querySelector('section.funcoes').getBoundingClientRect().top},
+      {secao: 'importacao', valor: document.querySelector('section.importacao').getBoundingClientRect().top},
+      {secao: 'demonstracao', valor: document.querySelector('section.demonstracao').getBoundingClientRect().top},
+      {secao: 'diagnostico', valor: document.querySelector('section.diagnostico').getBoundingClientRect().top},
+      {secao: 'contato', valor: document.querySelector('section.contato').getBoundingClientRect().top},
+    ]
+
+    const valor = 0;
+    const maisProximo = array.reduce((a, b) => {return (Math.abs(a.valor - valor) < Math.abs(b.valor - valor) ? a : b)});
+    
+    limparClassesItensSumario();
+    marcarItemAtivo(maisProximo.secao);
+  });
+
+  document.querySelector('.conteudo-sumario').addEventListener('click', () => {
+    verificarMudancaSecao();
+  })
+  
+  const limparClassesItensSumario = () => {
     const sumario = document.querySelector('.sumario__lista');
     
     const linksSumario = sumario.querySelectorAll('[data-navegacao]');
     linksSumario.forEach(link => {
       link.classList.value = 'sumario__item__link';
     })
-    
-    const interacaoValida = secaoNavegadaURL !== '' && secoesRolagem.includes(secaoNavegadaURL);
-    
-    if(interacaoValida){
-      const secaoAtiva = document.querySelectorAll(`[data-navegacao="${secaoNavegadaURL}"]`);
-      secaoAtiva.forEach(secao => {
-        secao.classList.toggle('ativo');
-      })
-    }
   }
-  
-  document.querySelector('.conteudo-sumario').addEventListener('click', () => {
-    verificarMudancaSecao();
-  })
-  
+
+  const marcarItemAtivo = (secao) => {
+    const secaoAtiva = document.querySelectorAll(`[data-navegacao="${secao}"]`);
+    secaoAtiva.forEach(secao => {
+      secao.classList.add('ativo');
+    })
+  }
+
   const acoesNavegacao = document.querySelectorAll('[data-acao-navegacao]');
   acoesNavegacao.forEach(acao => {
     
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,31 +13,4 @@
     const overlay2 = document.querySelector(".overlay-2");
     overlay2.style.display = "none";
   })
-
-  window.addEventListener('scroll', () => {
-    const posicao = Math.floor(window.scrollY);
-
-    const cabecalho = document.querySelector('header.cabecalho').getBoundingClientRect().top;
-    const funcoes = document.querySelector('section.funcoes').getBoundingClientRect().top;
-    const importacao = document.querySelector('section.importacao').getBoundingClientRect().top;
-    const demonstracao = document.querySelector('section.demonstracao').getBoundingClientRect().top;
-    const diagnostico = document.querySelector('section.diagnostico').getBoundingClientRect().top;
-    const contato = document.querySelector('section.contato').getBoundingClientRect().top;
-
-    const array = [
-      {secao: 'cabecalho', valor: cabecalho},
-      {secao: 'funcoes', valor: funcoes},
-      {secao: 'importacao', valor: importacao},
-      {secao: 'demonstracao', valor: demonstracao},
-      {secao: 'diagnostico', valor: diagnostico},
-      {secao: 'contato', valor: contato},
-    ]
-
-    const valor = 0;
-    const maisProximo = array.reduce((a, b) => {return (Math.abs(a.valor - valor) < Math.abs(b.valor - valor) ? a : b)});
-    console.log(maisProximo.secao);
-
-    
-
-  });
 })();
